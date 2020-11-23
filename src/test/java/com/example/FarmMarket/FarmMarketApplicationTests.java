f7c060c2954a6ac62fdd9f47dcaf7e3391e9e207
igor <igorvinogradov87@gmail.com>
2020-11-23 13:57:47 +0200

    Initial commit

---
+++ src/test/java/com/example/FarmMarket/FarmMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FarmMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
