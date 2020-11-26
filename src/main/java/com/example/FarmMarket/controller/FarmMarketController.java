875f9be6db7a1fe7928968bb9ce88e4911493b29
anna lazarenkova <anna.lazarenkova@gmail.com>
2020-11-26 11:36:59 +0200

    tabelite uuendus 26.11

--- src/main/java/com/example/FarmMarket/controller/FarmMarketController.java
+++ src/main/java/com/example/FarmMarket/controller/FarmMarketController.java
@@ -1,10 +1,10 @@
 package com.example.FarmMarket.controller;
 
 
+import com.example.FarmMarket.Seller;
 import com.example.FarmMarket.service.FarmMarketService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
 
@@ -14,8 +14,8 @@ public class FarmMarketController {
     private FarmMarketService farmMarketService;
 
     @PostMapping("newSeller")
-    public void newSeller (String name, String personal_information, String address, String phone) {
-        farmMarketService.newSeller(name, personal_information, address, phone);
+    public void newSeller (@RequestBody Seller seller) {
+        farmMarketService.newSeller(seller.getName(), seller.getEmail(), seller.getUsername(), seller.getPassword(), seller.getPhone());
     }
 
     @PostMapping("newProduct")
@@ -23,7 +23,40 @@ public class FarmMarketController {
                             BigInteger price, BigInteger amount) {
         farmMarketService.newProduct(category, product_name, product_description, price, amount);
     }
+    @PutMapping("updateSellerName")
+    public void updateSellerName(@RequestBody Seller seller){
+        farmMarketService.updateSellerName(seller.getId(), seller.getName());
+    }
+
+    @PutMapping("updateSellerEmail")
+    public void updateSellerEmail(@RequestBody Seller seller){
+        farmMarketService.updateSellerEmail(seller.getId(), seller.getEmail());
+    }
+
+    @PutMapping("updateSellerUsername")
+    public void updateSellerUsername(@RequestBody Seller seller){
+        farmMarketService.updateSellerUsername(seller.getId(), seller.getUsername());
+    }
 
 
+    @PutMapping("updateSellerPersonalInformation")
+    public void updateSellerPersonalInformation(@RequestBody Seller seller){
+        farmMarketService.updateSellerPersonalInformation(seller.getId(), seller.getPersonalInformation());
+    }
+
+    @PutMapping("updateSellerAddress")
+    public void updateSellerAddress(@RequestBody Seller seller){
+        farmMarketService.updateSellerAddress(seller.getId(), seller.getAddress());
+    }
+
+    @PutMapping("updateSellerPhone")
+    public void updateSellerPhone(@RequestBody Seller seller){
+        farmMarketService.updateSellerPhone(seller.getId(), seller.getPhone());
+    }
+
+    @PutMapping("updateSellerPassword")
+    public void updateSellerPassword(@RequestBody Seller seller){
+        farmMarketService.updateSellerPassword(seller.getId(), seller.getPassword());
+    }
 
 }
--- src/main/java/com/example/FarmMarket/repository/FarmMarketRepository.java
+++ src/main/java/com/example/FarmMarket/repository/FarmMarketRepository.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,13 +14,14 @@ public class FarmMarketRepository {
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 
-    public void newSeller(String name, String personal_information, String address, String phone) {
-        String sql = "insert into seller(name, personal_information, address, phone) values(:name, :personal_information, :address, :phone)";
+    public void newSeller(String name, String email, String username, String password, String phone) {
+        String sql = "insert into seller(name, email, username, password, phone) values(:m1, :m2, :m3, :m4, :m5)";
         Map<String, Object> paramMap = new HashMap<>();
-        paramMap.put("name", name);
-        paramMap.put("personal_information", personal_information);
-        paramMap.put("address", address);
-        paramMap.put("phone", phone);
+        paramMap.put("m1", name);
+        paramMap.put("m2", email);
+        paramMap.put("m3", username);
+        paramMap.put("m4", password);
+        paramMap.put("m5", phone);
         jdbcTemplate.update(sql, paramMap);
     }
 
@@ -33,6 +35,61 @@ public class FarmMarketRepository {
         paramMap.put("product_description", product_description);
         paramMap.put("price", price);
         paramMap.put("amount", amount);
+        jdbcTemplate.update(sql, paramMap);
+    }
+
+    public void updateSellerName(int ID, String newName){
+        String sql = "UPDATE seller SET name =:m1 WHERE id = :m2";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("m1", newName);
+        paramMap.put("m2", ID);
+        jdbcTemplate.update(sql, paramMap);}
+
+    public void updateSellerPersonalInformation(int ID, String personalInformation){
+        String sql = "UPDATE seller SET personal_information =:m1 WHERE id = :m2";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("m1", personalInformation);
+        paramMap.put("m2", ID);
+        jdbcTemplate.update(sql, paramMap);
+    }
+
+    public void updateSellerAddress(int ID, String address){
+        String sql = "UPDATE seller SET address =:m1 WHERE id = :m2";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("m1", address);
+        paramMap.put("m2", ID);
+        jdbcTemplate.update(sql, paramMap);
+    }
+
+    public void updateSellerEmail(int ID, String email) {
+        String sql = "UPDATE seller SET email =:m1 WHERE id = :m2";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("m1", email);
+        paramMap.put("m2", ID);
+        jdbcTemplate.update(sql, paramMap);
+    }
+
+    public void updateSellerPhone(int ID, String phone) {
+        String sql = "UPDATE seller SET phone =:m1 WHERE id = :m2";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("m1", phone);
+        paramMap.put("m2", ID);
+        jdbcTemplate.update(sql, paramMap);
+    }
+
+    public void updateSellerUsername(int ID, String username) {
+        String sql = "UPDATE seller SET username =:m1 WHERE id = :m2";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("m1", username);
+        paramMap.put("m2", ID);
+        jdbcTemplate.update(sql, paramMap);
+    }
+
+    public void updateSellerPassword(int ID, String password) {
+        String sql = "UPDATE seller SET password =:m1 WHERE id = :m2";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("m1", password);
+        paramMap.put("m2", ID);
         jdbcTemplate.update(sql, paramMap);
     }
 }
