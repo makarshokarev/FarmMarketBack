d9aadc94b10e5872936c31c4c976aa3ce4557ad6
Makar Shokarev <makarshokarev@gmail.com>
2020-11-26 15:54:08 +0200

    new product function with sql

---
+++ src/main/java/com/example/FarmMarket/service/CategoryService.java
@@ -0,0 +1,19 @@
+package com.example.FarmMarket.service;
+
+import com.example.FarmMarket.objects.Category;
+import com.example.FarmMarket.repository.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepo categoryRepo;
+
+    public List<Category> getCategory(){
+        List<Category> result = categoryRepo.getCategory();
+        return result;
+    }
+}
